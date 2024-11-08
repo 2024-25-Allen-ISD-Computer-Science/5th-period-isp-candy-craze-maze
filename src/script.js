76f3838d3d28039c27548421a4e9935b96497104
aadhya-gaur <142256355+aadhya-gaur@users.noreply.github.com>
2024-11-08 16:10:16 +0000

    tried to make maze

---
+++ src/script.js
@@ -0,0 +1,23 @@
+const layout = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+    1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 
+    1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 
+    1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 
+    1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 
+    1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+];
+
+const gameContainer = document.getElementById("game");
+
+function createMaze() {
+    layout.forEach(cell => {
+        const div = document.createElement("div");
+        div.classList.add(cell === 1 ? "wall" : cell === 2 ? "collectible" : "path");
+        gameContainer.appendChild(div);
+    });
+}
+
+createMaze();
