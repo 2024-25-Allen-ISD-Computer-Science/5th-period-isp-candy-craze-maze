60e8f1d962b02a9b301b73b770992bdad9d2151b
advithibaddam <advithibaddam1@gmail.com>
2024-11-14 07:04:52 +0000

    tried to make movment but stil not working

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 const layout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
     1, 0, 0, 0, 0, 2, 0, 0, 0, 1,  
@@ -20,34 +21,109 @@ function createMaze() {
 }
 
 createMaze();
-//Ths is placing the character on the grid
 
-var spritobject = 
+var canvas = document.querySelector("canvas");
+var drawingSurface = canvas.getContext("2d");
+var spritobject =
 {
     x:0,
     y:0,
-    width: 64,
-    height: 64
-}
+    width: 47,
+    height: 40
+};
 
-document.addEventListener(keydown, (event) => {
-   const speed = 10;
 
-    switch (event.key) {
+var cake = Object.create(spriteObject);
+cake.x = 100;
+cake.y = 100;
+var image = new Image();
+image.addEventListener("load", loadHandler, false);
+image.src = "cake-pic.png";
+var Xspeed = 0;
+var Yspeed = 0;
+var moveLeft = false;
+var moveRight = false;
+var moveUp = false;
+var moveDown = false;
+
+
+
+
+window.addEventListener("keydown", function(e)
+{
+    switch (e.key)
+    {
        case "ArrowUp":
-           if (cakePosition.y > 0) cakePosition.y += 1;
+           moveUp = true;
            break;
        case "ArrowDown":
-           if (cakePosition.y > 0) cakePosition.y -= 1;
+           moveDown = true;
            break;
        case "ArrowLeft":
-           if (cakePosition.x > 0) cakePosition.x -= 1;
+           moveLeft = true;
            break;
        case "ArrowRight":
-           if (cakePosition.x < gridSize - 1) cakePosition.x += 1;
+           moveRight = true;
            break;
    }
-   updatePosition();
-      
-   }
-)
+     
+}, false);
+
+
+window.addEventListener("keyup", function(e)
+{
+    switch (e.key)
+    {
+       case "ArrowUp":
+           moveUp = false;
+           break;
+       case "ArrowDown":
+           moveDown = false;
+           break;
+       case "ArrowLeft":
+           moveLeft = false;
+           break;
+       case "ArrowRight":
+           moveRight = false;
+           break;
+   }  
+     
+}, false);
+
+
+function update()
+{
+    window.requestAnimationFrame(update);
+   
+    Xspeed = 0;
+    Yspeed = 0;
+
+
+    if(moveUp)
+        {
+            Yspeed = -5;
+        }
+    if(moveDown)
+        {
+            Yspeed = 5;
+        }
+    if(moveLeft)
+        {
+            Xspeed = -5;
+        }
+    if(moveRight)
+        {
+            Xspeed = 5;
+        }
+    cake.x += Xspeed;
+    cake.y += Yspeed;
+
+
+    drawingSurface.clearRect(0, 0, canvas.width, canvas,height);
+    drawingSurface.drawImage(image, cake.x, cake.y, cake.width, cake.height);
+}
+
+
+image.addEventListener("load", function() {
+    update();
+}, false);
